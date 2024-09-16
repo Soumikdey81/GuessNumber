631c3d1230aaa6555673f3c88e7cc55b1666e5e7
Soumikdey81 <aditdey30@gmail.com>
2024-09-16 11:00:15 +0530

    Soumik Dey

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "🥳 Correct Number!";
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 20;
-console.log(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️ No number!";
+  }
+});
